cee80aa3201fa00d0306dde7ed1772b0d7c0b458
takina888 <takinaito@gmail.com>
2026-08-02 20:34:08 +0800

    Delete start_server.py

--- start_server.py
+++
@@ -1,6 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-from pathlib import Path
-import os
-os.chdir(Path(__file__).resolve().parent)
-print('WORK PHOTO: http://localhost:8080')
-ThreadingHTTPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler).serve_forever()
